33aaf6a9b89f0c8c90e82849a55b5c384d236ca4
rabi-siddique <rabisiddique27@gmail.com>
2025-04-29 21:04:21 +0500

    chore: enforce a proposal shape

--- contract/stake.contract.js
+++ contract/stake.contract.js
@@ -21,12 +21,24 @@ const trace = makeTracer('StkC');
  * @import {Marshaller, StorageNode} from '@agoric/internal/src/lib-chainStorage.js';
  * @import {PortfolioConfig} from './typeGuards.js';
  * @import { ZCF } from '@agoric/zoe/src/zoeService/zoe.js';
+ * @import {Amount} from '@agoric/ertp';
+ */
+
+/**
+ * In addition to the standard `issuers` and `brands` terms,
+ * this contract is parameterized by terms for price and,
+ * optionally, a maximum number of items sold for that price (default: 3).
+ *
+ * @typedef {{
+ * fee: Amount;
+ * retainer: Amount;
+ * }} StkCTerms
  */
 
 /**
  * Orchestration contract to be wrapped by withOrchestration for Zoe
  *
- * @param {ZCF} zcf
+ * @param {ZCF<StkCTerms>} zcf
  * @param {OrchestrationPowers & {
  *   marshaller: Marshaller;
  *   chainInfo: Record<string, CosmosChainInfo>;
@@ -79,6 +91,12 @@ export const contract = async (
     log,
   });
 
+  const { fee, retainer } = zcf.getTerms();
+  const proposalShape = harden({
+    give: { Fee: M.eq(fee), Retainer: M.eq(retainer) },
+    exit: M.any(),
+  });
+
   const publicFacet = zone.exo(
     'Staking API',
     M.interface('Staking API', {
@@ -90,7 +108,11 @@ export const contract = async (
       /** @param {PortfolioConfig} config */
       makeStakingPortfolio(config) {
         trace('makeStakingPortfolio(', config, ')');
-        return zcf.makeInvitation(makeStakingPortfolio, 'makeLCA', undefined);
+        return zcf.makeInvitation(
+          makeStakingPortfolio,
+          'makeStakingPortfolio',
+          proposalShape,
+        );
       },
     },
   );
