124172206a5429a3cc7f672ecf70561499b8b125
Dan Connolly <dckc@madmode.com>
2025-04-29 14:20:24 -0500

    chore: proposal to make offer requires Fee
     - terms.Fee
       - customTermsShape for early detection of errors
       - privateArgsShape while we're at it
         - ChainInfo not limited to CosmosChainInfo
     - test: provide BLD issuer to contract
       - provide a BLD purse to executeOffer

--- contract/typeGuards.js
+++ contract/typeGuards.js
@@ -1,8 +1,17 @@
+import { AmountShape, RatioShape } from '@agoric/ertp/src/typeGuards.js';
+import {
+  ChainInfoShape,
+  DenomDetailShape,
+  DenomShape,
+  OrchestrationPowersShape,
+} from '@agoric/orchestration';
 import { M } from '@endo/patterns';
 
 /**
  * @import {TypedPattern} from '@agoric/internal';
  * @import {CopySet} from '@endo/patterns';
+ * @import {StakingTerms} from './stake.contract';
+ * @import {Brand, NatValue} from '@agoric/ertp';
  */
 
 /**
@@ -15,7 +24,7 @@ import { M } from '@endo/patterns';
  * @typedef {{ [chainName: string]: RemoteConfig}} PortfolioConfig
  */
 
-/** @type {TypedPattern<ProfileConfig>} */
+/** @type {TypedPattern<PortfolioConfig>} */
 export const PortfolioConfigShape = M.recordOf(
   M.string(),
   M.splitRecord(
@@ -27,3 +36,43 @@ export const PortfolioConfigShape = M.recordOf(
     },
   ),
 );
+
+/** @type {TypedPattern<StakingTerms>} */
+export const customTermsShape = M.splitRecord(
+  { portfolioFee: AmountShape },
+  { commission: RatioShape },
+);
+harden(customTermsShape);
+
+export const privateArgsShape = M.splitRecord(
+  {
+    // @ts-expect-error TypedPattern not recognized as record
+    ...OrchestrationPowersShape,
+    marshaller: M.remotable('Marshaller'),
+    chainInfo: M.recordOf(M.string(), ChainInfoShape),
+    storageNode: M.remotable('StorageNode'),
+  },
+  {
+    assetInfo: M.arrayOf([DenomShape, DenomDetailShape]),
+  },
+);
+harden(privateArgsShape);
+
+/**
+ * @param {Brand} brand
+ * @param {NatValue} [min]
+ */
+const makeAmountShape = (brand, min = undefined) =>
+  harden({ brand, amount: typeof min === 'undefined' ? M.nat() : M.gte(min) });
+
+/** @param {StakingTerms} ct */
+export const makeProposalShapes = (ct) => {
+  return harden({
+    makePortfolio: M.splitRecord({
+      give: M.splitRecord(
+        { Fee: M.gte(ct.portfolioFee) },
+        { Retainer: makeAmountShape(ct.portfolioFee.brand) },
+      ),
+    }),
+  });
+};
